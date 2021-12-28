0932e753f3026539223e36ceb337865bd954e45b
MontyPan <monty.pan@dontcareabout.us>
2021-12-29 01:07:58 +0800

    增加：name

--- src/main/java/us/dontcareabout/realtor/client/util/gf/Task.java
+++ src/main/java/us/dontcareabout/realtor/client/util/gf/Task.java
@@ -1,7 +1,13 @@
 package us.dontcareabout.realtor.client.util.gf;
 
 public abstract class Task {
+	public final String name;
+
 	private TaskFlow flow;
+
+	public Task(String name) {
+		this.name = name;
+	}
 
 	protected abstract void start();
 
