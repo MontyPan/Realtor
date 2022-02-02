5f1e384a030b8f642c4f131ad5f0989f0cb88d37
MontyPan <monty.pan@dontcareabout.us>
2022-02-03 00:59:29 +0800

    增加：讀取資料

--- src/main/java/us/dontcareabout/realtor/client/RealtorEP.java
+++ src/main/java/us/dontcareabout/realtor/client/RealtorEP.java
@@ -1,10 +1,19 @@
 package us.dontcareabout.realtor.client;
 
+import com.google.gwt.core.client.Scheduler;
 import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.ui.RootPanel;
 
 import us.dontcareabout.gst.client.GSTEP;
+import us.dontcareabout.gxt.client.component.RwdRootPanel;
+import us.dontcareabout.realtor.client.component.REGrid;
+import us.dontcareabout.realtor.client.component.gf.Loading;
+import us.dontcareabout.realtor.client.data.DataCenter;
+import us.dontcareabout.realtor.client.util.gf.TaskFlow;
 
 public class RealtorEP extends GSTEP {
+	private TaskFlow flow = new TaskFlow();
+
 	public RealtorEP() {
 		super("Realtor", "1mky8dxwveDYOg6qeelfePup33xly2LGJJDyBvSc7vQo");
 	}
@@ -22,5 +31,29 @@ public class RealtorEP extends GSTEP {
 
 	@Override
 	protected void start() {
+		RootPanel.get("loading").setVisible(false);
+
+		REGrid grid = new REGrid();
+		RwdRootPanel.setComponent(grid);
+
+		flow.onStart(Loading::show)
+			.onEnd(() -> {
+				Scheduler.get().scheduleFixedDelay(() -> {
+					Loading.hide();
+					grid.refresh(DataCenter.getAllRE());
+					return false;
+				}, 100);
+			})
+			.onTaskStart(() -> Loading.upgrade(flow.getCurrentTask().name))
+			.onTaskEnd(() -> Loading.upgrade(flow.getProgress()))
+		;
+
+		String[] tabName = {
+		};
+		for (String name : tabName) {
+			flow.add(new FetchRealEstateTask(name));
+		}
+
+		flow.start();
 	}
 }
